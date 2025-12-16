c0e6b2d5e6d46764c953a83583e333f4fe26f364
aysnigdha <anzuman.yasmin@gmail.com>
2025-12-16 13:12:15 +0600

    update node

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    "chromeWebSecurity": false ,// disable from iframe-heavy
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  },
-});
+     video: true, // ✅ Enable video recording
+      screenshotOnRunFailure: true, // optional: only take screenshots on failures
+      screenshotsFolder: 'cypress/screenshots',
+      videosFolder: 'cypress/videos'
+    },
+  });
+
